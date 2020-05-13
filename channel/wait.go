a2b06b47c3ff38bdc2e97ba1e2571badc6c784d4
ganyulong02 <ganyulong02@gmail.com>
2020-05-13 11:12:50 -0700

    compare

--- channel/wait.go
+++ channel/wait.go
@@ -16,3 +16,25 @@ func main() {
 func sendRPC(i int) {
 	println(i)
 }
+
+/*
+import "sync"
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		// This is a go routine
+		go func(x int) {
+			sendRPC(x)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
+
+func sendRPC(i int) {
+	println(i)
+}
+
+*/
